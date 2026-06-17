44b6c3e5c7820b59c18a4e2120fe30394a62da08
longisrad <long.israd@gmail.com>
2026-06-17 16:56:51 +0700

    Create sqm.sh

---
+++ trunk/user/sqm-qos/sqm.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+sqm_enable=$(nvram get sqm_enable)
+sqm_wan=$(nvram get sqm_wan_if)
+[ -z "$sqm_wan" ] && sqm_wan=$(nvram get wan0_ifname)
+[ -z "$sqm_wan" ] && sqm_wan="eth3" # Cổng WAN mặc định phổ biến trên MT7621
+
+sqm_dl=$(nvram get sqm_dl_speed)
+sqm_ul=$(nvram get sqm_ul_speed)
+sqm_qdisc=$(nvram get sqm_qdisc)
+[ -z "$sqm_qdisc" ] && sqm_qdisc="fq_codel"
+
+stop_sqm() {
+    logger -t "SQM" "Đang dừng SQM trên cổng $sqm_wan..."
+    tc qdisc del dev $sqm_wan root 2>/dev/null
+    tc qdisc del dev $sqm_wan ingress 2>/dev/null
+}
+
+start_sqm() {
+    [ "$sqm_enable" != "1" ] && return
+    [ -z "$sqm_dl" ] && sqm_dl="0"
+    [ -z "$sqm_ul" ] && sqm_ul="0"
+    
+    stop_sqm
+    
+    logger -t "SQM" "Đang khởi chạy SQM trên cổng $sqm_wan (Tải về: ${sqm_dl}Kbps, Tải lên: ${sqm_ul}Kbps) thuật toán: $sqm_qdisc..."
+    
+    # Cấu hình tối ưu băng thông tải lên (Upload) trên giao tiếp WAN
+    if [ "$sqm_ul" -gt 0 ]; then
+        tc qdisc add dev $sqm_wan root handle 1: htb default 11
+        tc class add dev $sqm_wan parent 1: classid 1:1 htb rate ${sqm_ul}kbit ceil ${sqm_ul}kbit
+        tc class add dev $sqm_wan parent 1:1 classid 1:11 htb rate ${sqm_ul}kbit ceil ${sqm_ul}kbit
+        tc qdisc add dev $sqm_wan parent 1:11 handle 11: $sqm_qdisc
+    fi
+}
+
+case "$1" in
+start)
+    start_sqm
+    ;;
+stop)
+    stop_sqm
+    ;;
+restart)
+    stop_sqm
+    sleep 1
+    start_sqm
+    ;;
+*)
+    echo "Usage: $0 {start|stop|restart}"
+    exit 1
+    ;;
+esac
